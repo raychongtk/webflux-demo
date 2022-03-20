38bfb89c3eb062b1133fc42b55556d4e4ebfb92b
raychong <raychongtk@gmail.com>
2022-03-20 11:54:49 +0800

    add 201 status code to book controller and stop returning book in create method

--- src/main/java/com/example/webfluxdemo/web/controller/BookController.java
+++ src/main/java/com/example/webfluxdemo/web/controller/BookController.java
@@ -4,10 +4,12 @@ import com.example.webfluxdemo.domain.Book;
 import com.example.webfluxdemo.service.BookService;
 import com.example.webfluxdemo.web.apipayload.CreateBookRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -32,8 +34,9 @@ public class BookController {
         return bookService.getAll();
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/api/v1/book")
-    public Mono<Book> create(@Valid @RequestBody CreateBookRequest request) {
-        return bookService.create(request.name, request.author);
+    public void create(@Valid @RequestBody CreateBookRequest request) {
+        bookService.create(request.name, request.author).subscribe();
     }
 }
